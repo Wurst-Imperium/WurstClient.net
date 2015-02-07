29c3f54ad375746bf9446d23139a94eb2934afa3
Alexander01998 <contact.wurstimperium@gmail.com>
2015-02-08 00:00:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wurst-Client website
+The **new** official website of the Wurst Client. <http://new.wurst-client.tk/>
---
+++ js/scrolling.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function()
+{
+	$(window).scroll(function()
+	{
+		if($("#header").offset().top > 50)
+		{
+			$("#header").addClass("collapse");
+		}else
+		{
+			$("#header").removeClass("collapse");
+		}
+	});
+	
+	$('a[href*=#]:not([href=#])').click(function()
+	{
+		if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+		{
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if(target.length)
+			{
+				$('html, body').animate(
+				{
+					scrollTop: target.offset().top
+				}, 1500);
+			}
+		}
+	});
+	$('a[href=#]').click(function()
+	{
+		if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+		{
+			$('html, body').animate(
+			{
+				scrollTop: 0
+			}, 1500);
+		}
+	});
+});
