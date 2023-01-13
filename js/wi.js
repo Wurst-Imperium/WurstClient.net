a94addd0294e09b073eccfb32f85f539704a09bd
Alexander01998 <contact.wurstimperium@gmail.com>
2023-01-13 17:04:05 +0100

    Fix custom events

--- js/wi.js
+++ js/wi.js
@@ -4568,3 +4568,38 @@ $.widget( "metro.tile" , {
  return $.Metro.init();
 
 }));
+
+/* Custom Plausible events */
+let links = document.querySelectorAll("a[data-analytics]");
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', handleLinkEvent);
+    links[i].addEventListener('auxclick', handleLinkEvent);
+}
+function handleLinkEvent(event) {
+    var link = event.target;
+    var middle = event.type == "auxclick" && event.which == 2;
+    var click = event.type == "click";
+    while (link && (typeof link.tagName == 'undefined' || link.tagName.toLowerCase() != 'a' || !link.href)) {
+        link = link.parentNode;
+    }
+    if (middle || click) {
+        let eventName = link.getAttribute('data-analytics');
+        let eventProps = {};
+        let eventPropsAttrs = link.attributes;
+        for (var i = 0; i < eventPropsAttrs.length; i++) {
+            if(eventPropsAttrs[i].name.startsWith("data-analytics-")){
+                let propName = eventPropsAttrs[i].name.replace("data-analytics-","");
+                eventProps[propName] = eventPropsAttrs[i].value;
+            }
+        }
+        plausible(eventName, {props: eventProps});
+    }
+    if (!link.target) {
+        if (!(event.ctrlKey || event.metaKey || event.shiftKey) && click) {
+            setTimeout(function () {
+                location.href = link.href;
+            }, 150);
+            event.preventDefault();
+        }
+    }
+}
